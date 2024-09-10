6f21627c38b2f1e68d1e0a49ab03531629195f65
Mahfuzullah Galib <progressive.galib@gmail.com>
2024-09-10 10:33:19 +0600

    1st

---
+++ src/main/kotlin/com/game/Score/ScoreApplication.kt
@@ -0,0 +1,11 @@
+package com.game.Score
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/game/Score/ScoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.game.Score
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
